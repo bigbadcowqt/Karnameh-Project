deb4b017fd0853eab7663802268185d47c5f3af6
bigbadcow <60196518+bigbadcowqt@users.noreply.github.com>
2026-05-02 22:57:32 +0330

    Create mashin hesab.cpp

---
+++ mashin hesab.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+	float a, b;
+	for(int a=1; a<=10;){
+		for(int b=1; b<=10;){
+			cout<<"				salam in yek mashin hesab ast."<<'\n';
+			cout<<"lotfan adad aval ra vared konid:     ";
+			cin>>		a;
+			cout<<"lotfan adad dovom ra vared konid:	";
+			cin>>		b;
+			cout<<".........javab =			"<<a*b<<'\n';
+		}
+	}
+}
