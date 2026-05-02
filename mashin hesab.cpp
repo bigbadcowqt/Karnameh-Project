1ebfd27dc09cd24d2276438310a6f5b86a98b632
bigbadcow <60196518+bigbadcowqt@users.noreply.github.com>
2026-05-03 02:38:40 +0330

    Update mashin hesab.cpp

--- mashin hesab.cpp
+++ mashin hesab.cpp
@@ -1,26 +1,53 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    float a, b;
-    char ch;
+int main()
+{
+float a, b;
+char end;
+char op;
 
-    while(true) {
+while(true){
+cout<<"......::..MASHIN HESABE MICHOO..::......
+cout<<"adad aval :   ";
+cin>>a;
 
-        cout<<"adad aval: ";
-        cin>>a;
+cout<<endl<<"che amaliyati anjam bedam? ( + , - , * , / )";
+cin>>op;
 
-        cout<<"adad dovom: ";
-        cin>>b;
+cout<<endl<<"adad dovom:   ";
+cin>>b;
 
-        cout<<"javab = "<<a*b<<endl;
+cout<<endl;
 
-        cout<<"edame bedim? (y/n): ";
-        cin>>ch;
+if (op == '+')
+cout<<"javab=   "<<a+b<<endl;
 
-        if(ch=='n')
-            break;
-    }
+else if (op == '-')
+cout<<"javab=   "<<a-b<<endl;
 
-    return 0;
+else if (op == '*')
+cout<<"javab=   "<<a*b<<endl;
+
+else if (op == '/')
+{
+    if(b == 0)
+        cout<<"khata: taghsim bar sefr momken nist!"<<endl;
+    else
+        cout<<"javab=   "<<a/b<<endl;
+}
+
+else
+cout<<" amaliyat eshtebah ast!";
+
+cout<<endl;
+cout<<"......................"<<endl;
+
+cout << "edameh bedin?  (y/n): ";
+cin>>end;
+
+if(end=='n')
+break;
+
+}
 }
